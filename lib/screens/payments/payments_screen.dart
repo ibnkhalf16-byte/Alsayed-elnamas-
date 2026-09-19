61ade8c6249121716ede684b3ada5da553d4b850
ibnkhalf16-byte <ibnkhalf16@gmail.com>
2026-09-19 22:59:35 +0300

    Update payments_screen.dart

--- lib/screens/payments/payments_screen.dart
+++ lib/screens/payments/payments_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
-import 'package:intl/intl.dart';
+import 'package:intl/intl.dart' hide TextDirection;
 import '../../core/database/database_helper.dart';
 import '../../core/constants/app_colors.dart';
 import '../../core/utils/app_formatters.dart';
