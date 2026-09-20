c6d1f4bd65caeb2989eacfd9d075b0ceffb9de89
ibnkhalf16-byte <ibnkhalf16@gmail.com>
2026-09-20 23:37:19 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ void main() async {
   // تهيئة الاتصال بقاعدة بيانات Supabase بدلاً من SQLite
   await Supabase.initialize(
     url: 'https://eeqgzwsyzoitpjrlgohx.supabase.co',
-    anonKey: 'Sb_publishable_y5EyMhMxk4XlXtZKmcikaA_nR7be8Xv', // تم التصحيح من publishableKey إلى anonKey
+    anonKey: 'sb_publishable_y5EyMhMxk4XlXtZKmcikaA_nR7be8Xv', // تم التصحيح من publishableKey إلى anonKey
   );
 
   runApp(const ElsayedAccountsApp());
