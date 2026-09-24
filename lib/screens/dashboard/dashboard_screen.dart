bbfcaa7173b5ca4f21167a673a2d14c9dc1857ea
ibnkhalf16-byte <ibnkhalf16@gmail.com>
2026-09-24 22:09:55 +0300

    Update dashboard_screen.dart
    [skip ci]

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -157,7 +157,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
       textDirection: TextDirection.rtl,
       child: Scaffold(
         appBar: AppBar(
-          title: const Text('حسابات علاء ابو شادي', style: TextStyle(fontWeight: FontWeight.bold)),
+          title: const Text('حسابات السيد النماس', style: TextStyle(fontWeight: FontWeight.bold)),
           centerTitle: true,
           actions: [
             IconButton(
